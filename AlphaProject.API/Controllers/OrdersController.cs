71945ffde71ded50461c1c6a34b18cc25dfc6ea5
Alessandro <aberti77@gmail.com>
2025-08-24 10:14:01 +0200

    Implementazione CRUD ordini e DTOs
    Implementata logica CRUD per gli ordini in `OrdersController.cs` con mappatura tramite AutoMapper.
    Creati i DTO `ClientDto` e `OrderDto` con le relative proprietà e annotazioni di convalida.

---
+++ AlphaProject.API/Controllers/OrdersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AlphaProject.API.Models;
+using AlphaProject.Shared.Dtos;
+using AutoMapper;
+
+namespace AlphaProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private IMapper _mapper;
+
+        public OrdersController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrders()
+        {
+            var orders= await _context.Orders
+                .Include(oi=>oi.OrderItems) // include orderitems to fill the navigation property
+                .ToListAsync();
+
+            return Ok(_mapper.Map<List<OrderDto>>(orders));
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDto>> GetOrder(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.OrderItems)
+                .FirstOrDefaultAsync(o => o.OrderId == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<OrderDto>(order);
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, OrderDto orderDto)
+        {
+            if (id != orderDto.OrderId)
+            {
+                return BadRequest();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(orderDto, order);
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrderDto>> PostOrder(OrderDto orderDto)
+        {
+
+            Order order = _mapper.Map<Order>(orderDto);
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+           var respOrderDto = _mapper.Map<OrderDto>(order);
+
+            return CreatedAtAction("GetOrder", new { id = order.OrderId }, respOrderDto);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
---
+++ AlphaProject.Shared/Dtos/OrderDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaProject.Shared.Dtos
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "L'ID del cliente è obbligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "L'ID del cliente deve essere positivo")]
+        public int ClientId { get; set; }
+
+        [Required(ErrorMessage = "La data dell'ordine è obbligatoria")]
+        public DateTime OrderDate { get; set; }
+
+        [Required(ErrorMessage = "L'importo totale è obbligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "L'importo totale deve essere maggiore di 0")]
+        public decimal TotalAmount { get; set; }
+
+        // Navigation property - lista degli articoli dell'ordine
+        public List<OrderItemDto>? OrderItems { get; set; }
+    }
+}
