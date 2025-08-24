bc0a502a68a33786c0c24bfb9a7a7a996d463893
Alessandro <aberti77@gmail.com>
2025-08-24 10:14:01 +0200

    Implementazione CRUD ordini e DTOs
    Implementata logica CRUD per gli ordini in `OrdersController.cs` con mappatura tramite AutoMapper.
    Creati i DTO `ClientDto` e `OrderDto` con le relative proprietà e annotazioni di convalida.

--- AlphaProject.API/Mappings/MappingProfile.cs
+++ AlphaProject.API/Mappings/MappingProfile.cs
@@ -3,19 +3,30 @@
     using AlphaProject.API.Models;
     using AlphaProject.Shared.Dtos;
     using AutoMapper;
-    
+
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             CreateMap<Product, ProductDto>()
-                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems))
-                .ReverseMap();
+                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
+            //.ReverseMap();
 
-            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>();//.ReverseMap();
+            CreateMap<Client, ClientDto>();//.ReverseMap();
 
-            /*CreateMap<Client, ClientDto>().ReverseMap();
-            CreateMap<Order, OrderDto>().ReverseMap();*/
+            CreateMap<Order, OrderDto>()
+                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
+            //.ReverseMap();
+
+            CreateMap<OrderDto, Order>()
+                .ForMember(dest => dest.Client, opt => opt.Ignore())
+                .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
+
+            CreateMap<OrderItemDto, OrderItem>()
+                .ForMember(dest => dest.OrderItemId, opt => opt.Ignore())
+                .ForMember(dest => dest.Order, opt => opt.Ignore())
+                .ForMember(dest => dest.Product, opt => opt.Ignore());
         }
     }
 }
