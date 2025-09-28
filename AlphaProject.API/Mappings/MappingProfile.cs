58788a01aed156920a78ae96fcf4e82048002a8d
Alessandro <aberti77@gmail.com>
2025-09-28 15:01:29 +0200

    corretto mapping nel Product

--- AlphaProject.API/Mappings/MappingProfile.cs
+++ AlphaProject.API/Mappings/MappingProfile.cs
@@ -10,19 +10,23 @@
         {
             CreateMap<Product, ProductDto>()
                 .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
-            //.ReverseMap();
-
-            CreateMap<OrderItem, OrderItemDto>();//.ReverseMap();
+           
+            CreateMap<ProductDto, Product>()
+               .ForMember(dest => dest.OrderItems, opt => opt.Ignore()); // gestisci la logica se vuoi mappare anche gli OrderItems
+                       
             CreateMap<Client, ClientDto>();//.ReverseMap();
 
             CreateMap<Order, OrderDto>()
                 .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
             //.ReverseMap();
 
+            // Mapping tra OrderDto e Order. gli orderitems vengono mappati dal DTO al modello OrderItem
             CreateMap<OrderDto, Order>()
                 .ForMember(dest => dest.Client, opt => opt.Ignore())
                 .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
 
+            // Mapping tra OrderItem e OrderItemDto
+            CreateMap<OrderItem, OrderItemDto>();
             CreateMap<OrderItemDto, OrderItem>()
                 .ForMember(dest => dest.OrderItemId, opt => opt.Ignore())
                 .ForMember(dest => dest.Order, opt => opt.Ignore())
--- AlphaProject.Shared/Dtos/ProductDto.cs
+++ AlphaProject.Shared/Dtos/ProductDto.cs
@@ -14,6 +14,8 @@ namespace AlphaProject.Shared.Dtos
         public decimal Price { get; set; }
         public int Stock { get; set; }  
 
+        public string Description { get; set; } = "";
+
         public List<OrderItemDto>? OrderItems { get; set; }
     }
     
